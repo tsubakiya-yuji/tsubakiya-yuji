23d05fb5aad017a4c65dec08ae221e20abdd9a85
nehiro <hiroaki.nerio@gmail.com>
2024-01-02 23:13:13 +0900

    fix: github pages settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
 
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+	site: 'https://tsubakiya-yuji.github.io/',
+	outDir: './docs',
+	base: '/tsubakiya-yuji',
+	integrations: [tailwind()],
 });
